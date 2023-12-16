13b759a3b0f72befe4615702038c1a1a486c8b13
Suraj <sujjeee@outlook.com>
2023-12-17 02:04:09 +0530

    update getpackageinfo

--- src/utils/get-package-info.ts
+++ src/utils/get-package-info.ts
@@ -1,9 +1,9 @@
+import fs from "node:fs";
 import path from "path";
-import fs from "fs-extra";
 import type { PackageJson } from "type-fest";
 
 export function getPackageInfo() {
   const packageJsonPath = path.join("package.json");
 
-  return fs.readJSONSync(packageJsonPath) as PackageJson;
+  return JSON.parse(fs.readFileSync(packageJsonPath, "utf-8")) as PackageJson;
 }

c5b12619f079d892bd7c927b3aa0e91808827c3f
Suraj <sujjeee@outlook.com>
2023-12-17 16:53:47 +0530

    update getPackageInfo

--- src/utils/get-package-info.ts
+++ src/utils/get-package-info.ts
@@ -1,9 +1,7 @@
-import fs from "node:fs";
-import path from "path";
+import packageJson from "package-json";
 import type { PackageJson } from "type-fest";
 
-export function getPackageInfo() {
-  const packageJsonPath = path.join("package.json");
-
-  return JSON.parse(fs.readFileSync(packageJsonPath, "utf-8")) as PackageJson;
+export async function getPackageInfo() {
+  const packageInfo = await packageJson("dubco");
+  return packageInfo as PackageJson;
 }
